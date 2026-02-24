666f851bae31fa6bd035a1ae5eec52d5ef7d9ec4
Armasem15 <marcos.walger@escola.pr.gov.br>
2026-02-24 09:48:28 -0300

    Add files via upload

--- media_se.js
+++ media_se.js
@@ -7,10 +7,10 @@ let media = 0;
 let result = " ";
 
 media = (N1 + N2 + N3 + N4)/4;
-if (media > = 70){
+if (media>= 70) {
     result = "arpovado"
 } else {
     result = "reprovado"
 }
 
-console.log ("O aluno"+ aluno +"tem a media"+media + "esta"; + result);
+console.log ("O aluno" +aluno+ "tem a media" + media + "esta;" + result);
